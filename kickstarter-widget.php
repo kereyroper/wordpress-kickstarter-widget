c6a515733c3e36023b1ed62cb8f00abc4fcbbe07
Kerey Roper <kerey.roper@gmail.com>
2013-06-14 01:00:44 +0200

    stub php for widget

---
+++ kickstarter-widget.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Kickstater Widget
+Plugin URI: https://github.com/kereyroper/wordpress-kickstarter-widget
+Description: Adds a Kickstarter widget for your project suitable for sidebar
+Version: 0.1
+Author: Kerey Roper
+Author URI: http://www.rivetinggames.com/
+License: GPL2
+*/
+?>
+<?php
+/*  Copyright 2013 Kerey Roper (email : wordpress@rivetinggames.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
